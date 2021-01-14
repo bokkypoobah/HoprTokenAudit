f9ac934bf6ad59fc123a55c97c1819448007e2e7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-01-15 08:51:01 +1100

    Testing WIP

--- test/TestHoprToken.js
+++ test/TestHoprToken.js
@@ -8,7 +8,7 @@ const util = require('util');
 let InterestUtils;
 
 describe("TestHoprToken", function() {
-  let owner, user0, user1, hoprToken;
+  let owner, user0, user1, hoprToken, hoprDistributor;
 
   function printEvents(contract, receipt) {
     receipt.logs.forEach((log) => {
@@ -36,6 +36,7 @@ describe("TestHoprToken", function() {
     await singletons.ERC1820Registry(owner)
 
     HoprToken = await ethers.getContractFactory("HoprToken");
+    HoprDistributor = await ethers.getContractFactory("HoprDistributor");
     console.log("    owner -> HoprToken.deploy()");
     hoprToken = await HoprToken.deploy();
     const deployHoprTokenTransactionReceipt = await hoprToken.deployTransaction.wait();
@@ -43,16 +44,22 @@ describe("TestHoprToken", function() {
     console.log("    owner -> hoprToken.grantRole(MINTER_ROLE, owner)");
     const grantRole1 = await hoprToken.grantRole(await hoprToken.MINTER_ROLE(), owner);
     printEvents(hoprToken, await grantRole1.wait());
+
+    const now = parseInt(new Date() / 1000);
+    const maxMintAmount = ethers.utils.parseUnits("123", 18);
+    console.log("    owner -> HoprDistributor.deploy(hoprToken, " + now + ", " + ethers.utils.formatUnits(maxMintAmount, 18) + ")");
+    hoprDistributor = await HoprDistributor.deploy(hoprToken.address, now, maxMintAmount);
+    printEvents(hoprDistributor, await hoprDistributor.deployTransaction.wait());
   })
 
 
   it("TestHoprToken - #0", async function() {
     // expect(await hoprToken.name()).to.be.equal('HOPR Token', 'wrong name');
-    console.log("    name: " + await hoprToken.name());
-    console.log("    symbol: " + await hoprToken.symbol());
-    console.log("    decimals: " + await hoprToken.decimals());
-    console.log("    granularity: " + await hoprToken.granularity());
-    console.log("    totalSupply: " + ethers.utils.formatUnits(await hoprToken.totalSupply(), 18));
+    console.log("    hoprToken.name: " + await hoprToken.name());
+    console.log("    hoprToken.symbol: " + await hoprToken.symbol());
+    console.log("    hoprToken.decimals: " + await hoprToken.decimals());
+    console.log("    hoprToken.granularity: " + await hoprToken.granularity());
+    console.log("    hoprToken.totalSupply: " + ethers.utils.formatUnits(await hoprToken.totalSupply(), 18));
 
     console.log("    owner -> hoprToken.mint(user0, 123, '0x00', '0x00')");
     const mint1 = await hoprToken.mint(user0, ethers.utils.parseUnits("123", 18), '0x00', '0x00');
@@ -61,10 +68,11 @@ describe("TestHoprToken", function() {
     console.log("    owner -> hoprToken.mint(user1, 0.123456789123456789, '0x01', '0x02')");
     const mint2 = await hoprToken.mint(user1, ethers.utils.parseUnits("0.123456789123456789", 18), '0x01', '0x02');
     printEvents(hoprToken, await mint2.wait());
-    console.log(    "totalSupply: " + ethers.utils.formatUnits(await hoprToken.totalSupply(), 18));
-    console.log(    "user0.balance: " + ethers.utils.formatUnits(await hoprToken.balanceOf(user0), 18));
-    console.log(    "user1.balance: " + ethers.utils.formatUnits(await hoprToken.balanceOf(user1), 18));
+    console.log("    hoprToken.totalSupply: " + ethers.utils.formatUnits(await hoprToken.totalSupply(), 18));
+    console.log("    hoprToken.user0.balance: " + ethers.utils.formatUnits(await hoprToken.balanceOf(user0), 18));
+    console.log("    hoprToken.user1.balance: " + ethers.utils.formatUnits(await hoprToken.balanceOf(user1), 18));
 
+    console.log("    hoprDistributor.MULTIPLIER: " + await hoprDistributor.MULTIPLIER());
     // Decimal.set({ precision: 30 });
     // const SECONDS_PER_DAY = 60 * 60 * 24;
     // const SECONDS_PER_YEAR = 365 * SECONDS_PER_DAY;
